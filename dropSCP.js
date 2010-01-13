23c388b9373665862f7a33e62f5ef1b7ba4d43af
unknown <Florian@.(none)>
2010-01-13 13:58:16 +0100

    clipboard support

--- dropSCP.js
+++ dropSCP.js
@@ -1,3 +1,7 @@
+/*
+ * dropSCP.js v 0.3 - Upload files via pscp, needs clip.exe
+ */
+
 var dropSCP = new dropSCP();
 dropSCP.copyit();
 
@@ -10,6 +14,8 @@ function dropSCP()  {
 	this.ds_options = "-batch -q"
 	// this is the folder where stuff is saved
 	this.ds_target  = "/home/user/tmp/"
+	// this is where it will end up
+	this.ds_url = "http://example.org/tmp/"
 	// path to the pscp binary
 	this.ds_pscp    = "c:\\Apps\\putty\\pscp.exe"
 
@@ -32,12 +38,21 @@ function dropSCP()  {
 		
 		var sCommand = this.ds_pscp + " -P " + this.ds_port + " " + this.ds_options + ' "' + sFileName + '" ' + this.ds_session + ":" + this.ds_target;
 		var sStatus  = 'copied "' + sFileName + '" to ' + this.ds_session + ':' + this.ds_port;
+		var sClip = 'cmd.exe /c echo ' + this.ds_url + this.strip(sFileName) + ' | %systemroot%\\system32\\clip.exe';
 		
 		this._e(sCommand);
 		var shell = WScript.CreateObject ("WScript.Shell");
 		shell.run(sCommand, 0, true);
+		shell.run(sClip, 0, true);
 		
 		WScript.echo(sStatus);
 		this._e("<COPYIT");
+	}
+
+	this.strip = function(sPath) {
+		var fso = new ActiveXObject("Scripting.FileSystemObject");
+		var sFileName = fso.GetFileName(sPath);
+	
+		return sFileName;
 	}
 }
